6920973dbfd585c7aa5adac90decf710c3c347ad
Suleiman Leadbitter <sul@premcom.co.uk>
2017-10-24 17:11:07 +0100

    set up gulp script task runners

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,88 @@
 // Gulp.js configuration
-var gulp = require('gulp');
-var uncss = require('gulp-uncss');
+var gulp        = require('gulp'),
+    uncss       = require('gulp-uncss'),
+    pug         = require('gulp-pug'),
+    data        = require('gulp-data'),
+    babel       = require('gulp-babel'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    imagemin    = require('gulp-imagemin'),
+    stylus      = require('gulp-stylus'),
+    postcss     = require('gulp-postcss'),
+    svgo        = require('postcss-svgo'),
+    clean       = require('gulp-clean'),
+    prettify    = require('gulp-prettify'),
+    uglify      = require('gulp-uglify'),
+    concat      = require('gulp-concat'),
+    browserSync = require('browser-sync')
+;
 
-// development mode?
-// devBuild = (process.env.NODE_ENV !== 'production'),
+var paths = {
+  scripts: 'src/scripts/*.coffee',
+  images: 'src/images/**/*',
+  fonts: 'src/fonts/**/*',
+  styles: 'src/styles/style.styl',
+  templates: 'src/templates/*.pug'
+};
 
-// folders
-//   folder = {
-//     src: 'src/',
-//     build: 'build/'
-//   }
-// ;
-
-gulp.task('default', function () {
-  return gulp.src('build/css/styles.css')
+gulp.task('styles', function () {
+  return gulp.src('src/styles/style.styl')
+    .pipe(sourcemaps.init())
+    .pipe(stylus())
     .pipe(uncss({
-      html: ['build/*.html', 'build/**/*.html']
+      html: ['dist/*.html', 'dist/**/*.html']
     }))
-    .pipe(gulp.dest('build/css/out'));
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('dist/css/'));
 });
+
+gulp.task('images', function () {
+  return gulp.src(paths.images)
+    // Pass in options to the task 
+    .pipe(imagemin({ optimizationLevel: 5 }))
+    .pipe(gulp.dest('dist/img'));
+});
+
+gulp.task('templates', function distHTML() {
+  return gulp.src('src/templates/*.pug')
+    .pipe(pug())
+    .pipe(gulp.dest('dist/'))
+});
+
+gulp.task('scripts', function () {
+  gulp.src('src/scripts/*.js')
+    .pipe(babel({
+      presets: ['env']
+    }))
+    .pipe(gulp.dest('dist/js/'))
+});
+
+gulp.task('browser-sync', function () {
+  var files = [
+    'dist/**/*.html',
+    'dist/**/*.html',
+    'dist/css/**/*.css',
+    'dist/img/**/*.svg',
+    'dist/img/**/*.jpg',
+    'dist/img/**/*.gif',
+    'dist/img/**/*.png',
+    'dist/js/**/*.js'
+  ];
+
+  browserSync.init(files, {
+    server: {
+      baseDir: './dist'
+    }
+  });
+});
+
+gulp.task('watch', function () {
+  gulp.watch(paths.scripts, ['scripts']);
+  gulp.watch(paths.images, ['images']);
+  gulp.watch(paths.fonts, ['fonts']);
+  gulp.watch(paths.styles, ['styles']);
+  gulp.watch(paths.templates, ['templates']);
+});
+
+gulp.task('default', ['watch', 'scripts', 'images', 'templates', 'styles', 'browser-sync']);
+
+
